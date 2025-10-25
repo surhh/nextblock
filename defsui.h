31ba9d9412a4682507f813a99ec3667fea43c2a2
surhh <109148999+surhh@users.noreply.github.com>
2025-10-25 18:50:36 +0400

    cosmetic change

--- defsui.h
+++ defsui.h
@@ -1,8 +1,8 @@
 #ifndef DEFSUI_H
 #define DEFSUI_H
 
-#define UPDATE_CHECK_URL        "https://raw.githubusercontent.com/surhh/nextblock/main/_lastversion.txt"
-#define UPDATE_DOWNLOAD_URL     "https://github.com/surhh/nextblock/releases/latest"
+#define UPDATE_CHECK_URL                    "https://raw.githubusercontent.com/surhh/nextblock/main/_lastversion.txt"
+#define UPDATE_DOWNLOAD_URL                 "https://github.com/surhh/nextblock/releases/latest"
 
 #define TOSTR(x)                            #x
 #define TOSTRING(x)                         TOSTR(x)
@@ -15,7 +15,7 @@
 #define NEXTBLOCK_VERSION_INT               TOSTRING(NEXTBLOCK_MAJOR_VERSION)TOSTRING(NEXTBLOCK_MINOR_VERSION)
 #define NEXTBLOCK_APP_FULL_NAME             NEXTBLOCK_APP_NAME " " NEXTBLOCK_VERSION
 
-#define APP_NAME_VERSION_1  "<html><head/><body><p><span style=\" font-size:16pt; font-weight:600;\">"
-#define APP_NAME_VERSION_2  "</span></p></body></html>"
+#define APP_NAME_VERSION_1                  "<html><head/><body><p><span style=\" font-size:16pt; font-weight:600;\">"
+#define APP_NAME_VERSION_2                  "</span></p></body></html>"
 
 #endif // DEFSUI_H
